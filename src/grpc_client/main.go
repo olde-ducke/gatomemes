93dd4d0bba931c556b89a0112ed9ced773266e1b
olde-ducke <johnysfao@gmail.com>
2022-03-18 14:18:05 +0300

    update usage info for client

--- src/grpc_client/main.go
+++ src/grpc_client/main.go
@@ -34,14 +34,21 @@ func usage() {
 grpc-client works with running server
 
 FLAGS:
-	-a		specify server address and port (default: "localhost:50051")
-	-s		source image url or base64 encoded data, only jpeg and png are supported
-	-t		text to draw over src, "@" will be replaced with "\n", up to 3 lines of text
-	-o		output file name, saves file in working dir output is always png (default: "out.png")
-	-h, --help	prints help message
+ -a             specify server address and port (default: "localhost:50051")
+ -s             source image url or base64 encoded image, only jpeg and png are supported (required)
+ -t             text to draw over src, "@" will be replaced with "\n", up to 3 lines of text
+ -o             output file name, saves file in working dir output is always png (default: "out.png")
+ -h,--help      prints help message
+    --fonts     prints fonts file names available on server
 
-DRAWING OPTIONS:
-	    --fonts prints fonts file names available on server
+DRAWING OPTIONS (optional):
+ -i,            use font with index i, font list can be obtained with --fonts flag
+    --fscale	font scale, scales counterintuitively, lower values give bigger glyphs
+    --fcolor	rgb hex color as string, i.e. "ffffff", also supports "random" value, which will give random color
+    --ocolor	same as above, but for outline
+    --oscale	same as font scale, but for outline
+    --nooutline	disable outline drawing
+    --distort	add noise to glyph coordinates
 	`)
 }
 
